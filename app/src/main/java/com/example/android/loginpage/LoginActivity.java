2a44e454656834ae8f7083f2fd40e3b9329e95f8
kedarSedai <kedar.sedai05@gmail.com>
2019-01-20 09:03:46 +0545

    changes

---
+++ app/src/main/java/com/example/android/loginpage/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.loginpage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = (Button) findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
